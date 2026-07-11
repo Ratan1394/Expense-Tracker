a22355ab37d16ae2c96388bd53b6c0f2c00304ac
Ratan kumar <ratankrc072@gmail.com>
2026-07-11 15:13:09 +0530

    Refactor imports in Home.js for better readability

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { APIUrl, handleError, handleSuccess } from '../utils';
 import { ToastContainer } from 'react-toastify';
